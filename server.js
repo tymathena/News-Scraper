57f3c2b8f0d5cffee28f4c02a95dd3b68eefd2a2
Tyler Mathena <tgmathena@crimson.ua.edu>
2019-06-30 20:03:43 -0400

    fixed deployment issues

--- server.js
+++ server.js
@@ -34,6 +34,51 @@ mongoose.connect("mongodb://localhost/onionscraper", { useNewUrlParser: true });
 // Routes
 require("./routes/api-routes.js")(app)
 
+//Initial scrape
+
+const scrape = (req, res) => {
+
+
+  axios.get("https://www.theonion.com/").then(function (response) {
+
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("section.content-meta__headline__wrapper").each(function (i, element) {
+          // Save an empty result object
+          var result = {};
+
+          // Add the text and href of every link, and save them as properties of the result object
+          result.title = $(this)
+              .children("a")
+              .text();
+          console.log(result.title)
+          result.link = $(this)
+              .children("a")
+              .attr("href");
+
+          // Create a new Article using the `result` object built from scraping
+          if (result.title !== "Continue Reading") {
+              db.article.create(result)
+                  .then(dbArticle => {
+                      // View the added result in the console
+                      console.log(dbArticle);
+                  })
+                  .catch(err => {
+                      // If an error occurred, log it
+                      console.log(err);
+                  });
+          }
+      });
+
+      // Send a message to the client
+      res.send("Scrape Complete");
+  });
+}
+
+scrape()
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
